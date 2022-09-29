28c81828c3045606495d60bd96c8edfec2653a97
juanca99min <juan.ortiz@99minutos.com>
2022-09-29 17:59:35 -0500

    Initial commit

---
+++ agave-xml/src/main/java/com/minutos/agave_xml/components/TestAgave.kt
@@ -0,0 +1,7 @@
+package com.minutos.agave_xml.components
+
+class TestAgave {
+    companion object {
+        const val TEST_CONST = "Juan Carlos Ortiz"
+    }
+}
---
+++ app/src/main/java/com/minutos/agave_android/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.minutos.agave_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.minutos.agave_xml.components.TestAgave
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
